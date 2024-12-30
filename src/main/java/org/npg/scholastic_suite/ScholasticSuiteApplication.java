3da1c8505fb7a6c3d3dd20acb782030abecbefe4
nicko <p.nickogamba@gmail.com>
2024-12-30 20:58:06 +0800

    init project

---
+++ src/main/java/org/npg/scholastic_suite/ScholasticSuiteApplication.java
@@ -0,0 +1,13 @@
+package org.npg.scholastic_suite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholasticSuiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScholasticSuiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/npg/scholastic_suite/ScholasticSuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.npg.scholastic_suite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholasticSuiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
